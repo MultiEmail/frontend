d4872942695c61041d758d34cfb3811417738cee
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-09-29 13:06:27 +0530

    Added framer-motion for sleek animations. Configured Tailwind and also added Comments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { motion } from 'framer-motion'
 
 const App = () => {
   return (
-    <div className='font-poppins flex flex-col justify-center w-screen h-screen items-center'>
+    <motion.div className='font-poppins flex flex-col justify-center w-screen h-screen items-center' initial={{opacity: 0,}} animate={{opacity: 1,}} transition={{duration: 1, }}>
       <div className='text-center justify-center'>
         <h1 className='text-4xl font-semibold'>Multimail</h1>
-        <p className='text-sm text-gray-500'>The page will be worked on after the design has been finalized.</p>
+        <p className='text-sm text-gray-500 p-2'>The page will be worked on after the design has been finalized.</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
