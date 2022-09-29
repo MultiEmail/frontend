357679d32541b8e36f5b37fbc4fa55a3d1ff785b
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-09-29 12:49:29 +0530

    Intialized Tailwind, React-Redux-Typescript via create-react-app and npm

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div className='font-poppins flex flex-col justify-center w-screen h-screen items-center'>
+      <div className='text-center justify-center'>
+        <h1 className='text-4xl font-semibold'>Multimail</h1>
+        <p className='text-sm text-gray-500'>The page will be worked on after the design has been finalized.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,tsx, js, ts}"],
+  theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+		fontFamily: {
+			sans: ['Inter var', 'sans-serif'],
+			serif: ['Georgia', 'serif'],
+			mono: ['Menlo', 'Monaco', 'Consolas', 'monospace'],
+			poppins: ['Poppins', 'sans-serif'],
+			roboto: ['Roboto', 'sans-serif'],
+		},
+		fontWeights: {
+			normal: 400,
+			medium: 500,
+			bold: 700,
+			black: 900,
+		},
+		extend: {},
+	},
+  plugins: [],
+}
