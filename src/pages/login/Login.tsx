25e3ed4338080b82d789ed973ada3bfdc3a4ffc5
Kanlsk <kantaphattongla@gmail.com>
2022-10-06 19:51:27 +0700

    feat: created UI for signup and login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,113 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Link } from "react-router-dom";
+
+//import photos
+import vector from "../../assets/photos/vector-login.svg";
+import logo from "../../assets/logos/icon-transparent.svg";
+
+//import icons
+import {
+  AiOutlineArrowLeft,
+  AiFillEye,
+  AiFillEyeInvisible,
+} from "react-icons/ai";
 
 const Login: FC = () => {
-	return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  //for submit by clicking an enter key
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter") {
+      if (email === "" || password === "") {
+        return;
+      } else {
+        console.log({
+          Email: email,
+          Password: password,
+        });
+      }
+    }
+  };
+
+  const passwordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <div className="absolute top-0 left-0 grid h-screen w-screen place-items-center bg-white font-poppins ">
+      <div className="relative flex h-auto w-[90vw] max-w-6xl flex-col items-center justify-center gap-4 bg-[#DBE2EF] py-10 md:h-[60vh] md:w-[70vw] md:flex-row md:justify-around">
+        {/*icon for redirect to home */}
+        <Link to="/" className="absolute top-2 left-2">
+          <AiOutlineArrowLeft className="text-xl" />
+        </Link>
+        {/*logo*/}
+        <div className="md:hidden w-fit items-center gap-1 flex">
+          <img src={logo} alt="logo" className="w-[50px]" />
+          <p className="text-lg">Multi Email</p>
+        </div>
+        {/*image section */}
+        <div className="flex h-[30vh] w-[90%] items-center md:w-[50%] max-w-sm">
+          <img src={vector} alt="vector"/>
+        </div>
+        {/*separate line*/}
+        <div className="h-[1px] w-[95%] rounded-full border-2 border-[#3f71af60] bg-[#6398da60] md:h-[30vh] md:w-[0px]"></div>
+        {/*form section */}
+        <div className="flex w-[90%] flex-col items-center gap-3 md:w-[40%] mt-2 md:mt-0">
+          {/*logo*/}
+          <div className="hidden w-fit items-center gap-1 md:flex">
+            <img src={logo} alt="logo" className="w-[40px]" />
+            <p>Multi Email</p>
+          </div>
+          {/*form*/}
+          <div className="flex w-[90%] flex-col gap-2 text-[#3F72AF]">
+            <label className="text-[14px]">Email</label>
+            <input
+              type="email"
+              onKeyPress={handleKeyPress}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="rounded-md px-2 py-1 text-[12px] placeholder-[#3f72af]"
+              placeholder="example@multiemail.us"
+            />
+          </div>
+          <div className="relative flex w-[90%] flex-col gap-2 text-[#3F72AF]">
+            <label className="text-[14px]">Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onKeyPress={handleKeyPress}
+              className="rounded-md px-2 py-1 text-[12px] placeholder-[#3f72af]"
+              placeholder="password"
+            />
+            {showPassword ? (
+              <AiFillEyeInvisible
+                className="absolute right-2 top-[60%]"
+                onClick={passwordVisibility}
+              />
+            ) : (
+              <AiFillEye
+                className="absolute right-2 top-[60%]"
+                onClick={passwordVisibility}
+              />
+            )}
+          </div>
+          <button className="rounded-md bg-[#5271ff] px-2 py-1 text-[14px] text-white">
+            Log In
+          </button>
+          <p className="text-[12px]">
+            Don't have an account yet?{" "}
+            <Link to="/signup" className="text-[#5271ff] underline">
+              Sign Up
+            </Link>{" "}
+            here
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,127 +1,235 @@
-import { FC, FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { FC, FormEvent, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { ISignupPayload, signupHandler } from "../../actions/auth.actions";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import useUpdateObjectState from "../../hooks/useUpdateObjectState";
-
+//import photos
+import vector from "../../assets/photos/vector-signup.svg";
+import logo from "../../assets/logos/icon-transparent.svg";
+//import icons
+import {
+  AiOutlineArrowLeft,
+  AiFillEye,
+  AiFillEyeInvisible,
+} from "react-icons/ai";
 /**
  * Signup page
  * @returns JSX.Element
  */
 const Signup: FC = () => {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-	/**
-	 * This state will contain formData which will be posted to backend
-	 * when user clicks `signup` button
-	 * @constant
-	 * @author aayushchugh
-	 */
-	const [formData, setFormData] = useState<ISignupPayload>({
-		username: "",
-		email: "",
-		password: "",
-		cpassword: "",
-	});
+  /**
+   * This state will contain formData which will be posted to backend
+   * when user clicks `signup` button
+   * @constant
+   * @author aayushchugh
+   */
+  const [formData, setFormData] = useState<ISignupPayload>({
+    username: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
 
-	const updateFormData = useUpdateObjectState<ISignupPayload>(setFormData);
+  const [error, setError] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
 
-	/**
-	 * This function will be called when form is submitted
-	 * @param e form event
-	 *
-	 * @author aayushchugh
-	 */
-	const submitHandler = async (e: FormEvent) => {
-		e.preventDefault();
-		try {
-			await dispatch(signupHandler(formData));
-			navigate(`/verification?email=${formData.email}`);
+  const updateFormData = useUpdateObjectState<ISignupPayload>(setFormData);
 
-			// TODO: show success message on UI
-		} catch (err) {
-			// TODO: show error on UI
-		}
-	};
+  /**
+   * This function will be called when form is submitted
+   * @param e form event
+   *
+   * @author aayushchugh
+   */
+  const submitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await dispatch(signupHandler(formData));
+      navigate(`/verification?email=${formData.email}`);
 
-	/*
+      // TODO: show success message on UI
+    } catch (err) {
+      // TODO: show error on UI
+    }
+  };
+
+  //show password in input field
+  const passwordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  //show confirm password in input field
+  const confirmPasswordVisibility = () => {
+    setShowConfirmPassword((prev) => !prev);
+  };
+
+  useEffect(() => {
+    //to detect the enter key on the keyboard
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+		console.log(formData)
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    //used for clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
+
+  /*
 	  TODO: Add validation for following fields
 	- username
-		- required
-		- should not be shorter than 3 characters
-		- should not be longer than 50 characters
+		- [x]required
+		- [x]should not be shorter than 3 characters
+		- [x]should not be longer than 50 characters
 		
 	- email
-		- required
-		- should be a valid email
+		- [x]required
+		- [x]should be a valid email
 
 	- password
-		- required
-		- minimum length of 6 characters
+		- [x]required
+		- [x]minimum length of 6 characters
 
 	- cpassword
-		- required
-		- minimum length of 6 characters
-		- password and cpassword should be same
+		- [x]required
+		- [x]minimum length of 6 characters
+		- [x]password and cpassword should be same
+	-features
+		- [x]enter key for submit
+		- [x]responsive design
 	 */
-	return (
-		<main>
-			<form onSubmit={submitHandler}>
-				<div>
-					<label htmlFor="username">Username: </label>
-					<input
-						id="username"
-						type="text"
-						placeholder="multiemail"
-						className="border-2"
-						value={formData.username}
-						onChange={e => updateFormData("username", e.target.value)}
-					/>
-				</div>
-				<div>
-					<label htmlFor="email">Email: </label>
-					<input
-						id="email"
-						type="email"
-						placeholder="info@multiemail.us"
-						className="border-2"
-						value={formData.email}
-						onChange={e => updateFormData("email", e.target.value)}
-					/>
-				</div>
-				<div>
-					<label htmlFor="password">Password: </label>
-					<input
-						id="password"
-						type="password"
-						placeholder="strongpassword"
-						className="border-2"
-						value={formData.password}
-						onChange={e => updateFormData("password", e.target.value)}
-					/>
-				</div>
-				<div>
-					<label htmlFor="cpassword">Confirm Password: </label>
-					<input
-						id="cpassword"
-						type="password"
-						placeholder="strongpassword"
-						className="border-2"
-						value={formData.cpassword}
-						onChange={e => updateFormData("cpassword", e.target.value)}
-					/>
-				</div>
-
-				<button
-					type="submit"
-					className="bg-primary text-white py-1 px-2 rounded"
-				>
-					Signup
-				</button>
-			</form>
-		</main>
-	);
+  return (
+    <div className="absolute top-0 left-0 grid h-screen w-screen place-items-center bg-white font-poppins">
+      <div className="relative flex h-auto w-[90vw] max-w-6xl flex-col items-center justify-center gap-4 bg-[#DBE2EF] py-10 md:h-[60vh] md:w-[70vw] md:flex-row md:justify-around">
+        {/*icon for redirect to home */}
+        <Link to="/" className="absolute top-2 left-2">
+          <AiOutlineArrowLeft className="text-xl" />
+        </Link>
+        {/*logo*/}
+        <div className="flex w-fit items-center gap-1 md:hidden">
+          <img src={logo} alt="logo" className="w-[50px]" />
+          <p className="text-lg">Multi Email</p>
+        </div>
+        {/*image section */}
+        <div className="flex h-[30vh] w-[70%] max-w-sm items-center md:w-[50%]">
+          <img src={vector} alt="vector" />
+        </div>
+        {/*separate line*/}
+        <div className="h-[1px] w-[95%] rounded-full border-2 border-[#3f71af60] bg-[#6398da60] md:h-[30vh] md:w-[0px]"></div>
+        <form className="flex w-[90%] flex-col items-center gap-3 md:w-[40%]" onSubmit={submitHandler}>
+		{/*form section */}
+          {/*logo*/}
+          <div className="hidden w-fit items-center gap-1 md:flex">
+            <img src={logo} alt="logo" className="w-[40px]" />
+            <p>Multi Email</p>
+          </div>
+          <div className="flex w-[90%] flex-col gap-2 text-[#3F72AF]">
+            <label className="text-[14px]" htmlFor="username">
+              Username:{" "}
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="multiemail"
+              className="rounded-md px-2 py-1 text-[12px] placeholder-[#3f72af]"
+              value={formData.username}
+              onChange={(e) => updateFormData("username", e.target.value)}
+            />
+          </div>
+          <div className="flex w-[90%] flex-col gap-2 text-[#3F72AF]">
+            <label className="text-[14px]" htmlFor="email">
+              Email:{" "}
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="info@multiemail.us"
+              className="rounded-md px-2 py-1 text-[12px] placeholder-[#3f72af]"
+              value={formData.email}
+              onChange={(e) => updateFormData("email", e.target.value)}
+            />
+          </div>
+          <div className="relative flex w-[90%] flex-col gap-2 text-[#3F72AF]">
+            <label className="text-[14px]" htmlFor="password">
+              Password:{" "}
+            </label>
+            <input
+              id="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="strongpassword"
+              className="rounded-md px-2 py-1 text-[12px] placeholder-[#3f72af]"
+              value={formData.password}
+              onChange={(e) => updateFormData("password", e.target.value)}
+            />
+            {showPassword ? (
+              <AiFillEyeInvisible
+                className="absolute right-2 top-[65%] cursor-pointer"
+                onClick={passwordVisibility}
+              />
+            ) : (
+              <AiFillEye
+                className="absolute right-2 top-[65%] cursor-pointer"
+                onClick={passwordVisibility}
+              />
+            )}
+          </div>
+          <div className="relative flex w-[90%] flex-col gap-2 text-[#3F72AF]">
+            <label className="text-[14px]" htmlFor="cpassword">
+              Confirm Password:{" "}
+            </label>
+            <input
+              id="cpassword"
+              type={showConfirmPassword ? "text" : "password"}
+              placeholder="confirm your password"
+              className="rounded-md px-2 py-1 text-[12px] placeholder-[#3f72af]"
+              value={formData.cpassword}
+              onChange={(e) => updateFormData("cpassword", e.target.value)}
+            />
+            {showConfirmPassword ? (
+              <AiFillEyeInvisible
+                className="absolute right-2 top-[65%] cursor-pointer"
+                onClick={confirmPasswordVisibility}
+              />
+            ) : (
+              <AiFillEye
+                className="absolute right-2 top-[65%] cursor-pointer"
+                onClick={confirmPasswordVisibility}
+              />
+            )}
+          </div>
+          <div className="flex items-center gap-2">
+            <input type="checkbox" />
+            <label className="text-[12px]">
+              I agree with the{" "}
+              <span className="text-[#5271ff] underline">term of services</span>
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="rounded bg-primary py-1 px-2 text-white"
+          >
+            Signup
+          </button>
+          <p className="text-[12px]">
+            Already have an account?{" "}
+            <Link to="/login" className="text-[#5271ff] underline cursor-pointer">
+              Log In
+            </Link>{" "}
+            here
+          </p>
+		</form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
