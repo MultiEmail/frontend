040d91a7f707511f3dac6cb95bf3ffc17fb3f4c7
Toby <40370496+NullableDev@users.noreply.github.com>
2022-10-09 15:38:17 +0200

    feat: added about us page (#80)
    * feat: about page cuz why noot
    
    * feat(blog): add comment section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //import pages & components
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
-import Verification from "./pages/verification/Verification";
-import Nav from "./components/nav/Nav";
-import NotFound from "./pages/not_found/Not_Found";
-import { FC } from "react";
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Verification from './pages/verification/Verification';
+import Nav from './components/nav/Nav';
+import NotFound from './pages/not_found/Not_Found';
+import { FC } from 'react';
+import About from './pages/about/about';
 const App: FC = () => {
 	return (
 		<div>
@@ -18,6 +19,7 @@ const App: FC = () => {
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<Signup />} />
 					<Route path="/verify" element={<Verification />} />
+					<Route path="/about" element={<About />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
