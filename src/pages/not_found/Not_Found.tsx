060873b2a329313e754139bebf60242357414399
Go0odLuck <krimseskarolari@gmail.com>
2022-10-09 18:52:17 +0300

    fix: notfound improvment

--- src/pages/not_found/Not_Found.tsx
+++ src/pages/not_found/Not_Found.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import not_found from '../../assets/photos/not_found.svg'
 const NotFound = () => {
     return (
-      <div className=' flex-col justify-center items-center h-screen absolute top-0 left-0 grid h-screen w-screen place-items-center bg-white font-poppins '>
+      <div className=' flex-col justify-center items-center h-screen absolute top-0 left-0 grid w-screen place-items-center bg-white font-poppins '>
         <img src={not_found} alt="" className='w-2/4'/>
         <p className='text-4xl my-3 text-[#5271FF] font-light'>404 Page Not Found</p>
         <p className='mb-3 text-1xl'>-- try using a new url --</p>
