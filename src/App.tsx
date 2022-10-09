e2ad990b48fc6858e0bb44504e26d09311bd1b89
DaatUserName <hopetobylol@gmail.com>
2022-10-09 14:26:44 +0200

    Revert "feat: about page"
    This reverts commit c615bdce4215b131d6f2a436919fd0132d694d63.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //import pages & components
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
-import Verification from './pages/verification/Verification';
-import Nav from './components/nav/Nav';
-import NotFound from './pages/not_found/Not_Found';
-import { FC } from 'react';
-import About from './pages/about/About';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Verification from "./pages/verification/Verification";
+import Nav from "./components/nav/Nav";
+import NotFound from "./pages/not_found/Not_Found";
+import { FC } from "react";
 const App: FC = () => {
 	return (
 		<div>
@@ -19,7 +18,6 @@ const App: FC = () => {
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<Signup />} />
 					<Route path="/verify" element={<Verification />} />
-					<Route path="/about" element={<About />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
