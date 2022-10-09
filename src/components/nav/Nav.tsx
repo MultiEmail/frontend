fdc4d25f09c6f665559d1aa451c2b318a1a86059
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-10-09 08:18:59 +0530

    feat: added /verify and fixed some nav issues

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,25 +1,34 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logos/icon-transparent.svg";
-
+import { useLocation } from "react-router-dom";
 /**
  * Nav component
  * @author Kan Halder
  */
 const Nav: FC = () => {
+
+	const location = useLocation();
+	
+	// This is a hacky way to get the current route.
+	// I'm not sure if there's a better way to do this.
+	const currentRoute = location.pathname.split("/")[1];
+	console.log(`Current route: ${currentRoute}`);
+
+
 	return (
-		<div className="font-poppins backdrop-blur-lg w-screen flex justify-between items-center px-4 border-b-2 border-gray-400 h-[3.5em]">
+		<div className="absolute flex flex-row font-poppins w-screen justify-between items-center px-4 h-[60px] backdrop-blur-lg shadow-lg no-select">
 			{/*logo section*/}
 			<Link to="/">
-				<div className="flex items-center gap-2">
+				<div className="flex flex-row items-center mx-2">
 					<img src={logo} alt="logo" className="w-[60px]" />
-					<p className="text-2xl text-white">Multi Email</p>
+					<p className={"text-xl " + (currentRoute.length > 0 ? " text-black" : "text-white" )}>Multi Email</p>
 				</div>
 			</Link>
 			{/*login & signup section*/}
 			<div className="flex gap-3">
 				<Link to="/signup">
-					<button className="bg-[#5271ff] px-3 py-1 rounded-md text-white">
+					<button className="bg-[#5271ff] px-4 py-1 rounded-md text-white ">
 						Sign Up
 					</button>
 				</Link>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,17 @@
 import { FC } from "react";
 import sample from "../../assets/photos/product-sample.png";
+import { motion } from "framer-motion";
 
 const Home: FC = () => {
 	return (
-		<div className="w-screen h-screen grid place-items-center font-poppins background">
+		<motion.div className="w-screen h-screen grid place-items-center font-poppins background" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{duration: 0.5,}}>
 			<div className="flex flex-col gap-10 items-center">
 				<p className="text-white text-4xl">
 					Welcome to <span className="text-[#5271ff]">Multi Email</span>
 				</p>
 				<img src={sample} alt="product-sample" />
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
