c8c1e3e8a2e125d6e979bcdcdd2e06d801e33c5c
Karol-Ari <55296886+Karol-Ari@users.noreply.github.com>
2022-10-09 09:32:29 +0300

    Fix: Changed nav bar design (#72)
    * fix: something that fixed
    
    * fix: nav bar design

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -8,23 +8,23 @@ import logo from "../../assets/logos/icon-transparent.svg";
  */
 const Nav: FC = () => {
 	return (
-		<div className="font-poppins backdrop-blur-lg w-screen flex justify-between items-center px-4 border-b-2 border-gray-400 h-[3.5em]">
+		<div className="font-poppins backdrop-blur-lg w-screen h-[80px] flex justify-between items-center px-4 border-b-2 border-[#DBE2EF] h-[3.5em]">
 			{/*logo section*/}
 			<Link to="/">
 				<div className="flex items-center gap-2">
 					<img src={logo} alt="logo" className="w-[60px]" />
-					<p className="text-2xl text-white">Multi Email</p>
+					<p className="text-2xl font-regular text-[#F9F7F7]">Multi Email</p>
 				</div>
 			</Link>
 			{/*login & signup section*/}
-			<div className="flex gap-3">
+			<div className="flex gap-3 font-light ">
 				<Link to="/signup">
-					<button className="bg-[#5271ff] px-3 py-1 rounded-md text-white">
+					<button className="bg-[#5271ff] px-4 py-1 rounded-md text-[#F9F7F7]">
 						Sign Up
 					</button>
 				</Link>
 				<Link to="/login">
-					<button className="bg-white text-black px-4 py-1 rounded-md">
+					<button className="bg-[#F9F7F7] text-[#112D4E] px-4 py-1 rounded-md">
 						Log In
 					</button>
 				</Link>
