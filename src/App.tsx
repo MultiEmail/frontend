90c46461157e2bca317937340c609b6f485e3a66
Ayush Chugh <ayushchugh2006@gmail.com>
2022-10-13 15:03:31 +0530

    fix: corrected patch of about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //import pages & components
 import Home from "./pages/home/Home";
@@ -9,19 +9,19 @@ import NavLayout from "./components/nav/NavbarLayout";
 import NotFound from "./pages/not_found/Not_Found";
 import { FC } from "react";
 
-import About from './pages/about/about';
+import About from "./pages/about/About";
 
 const App: FC = () => {
 	return (
 		<div>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<NavLayout/>}>
+					<Route path="/" element={<NavLayout />}>
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login />} />
 						<Route path="/signup" element={<Signup />} />
 						<Route path="/verify" element={<Verification />} />
-            			<Route path="/about" element={<About />} />
+						<Route path="/about" element={<About />} />
 						<Route path="*" element={<NotFound />} />
 					</Route>
 				</Routes>
