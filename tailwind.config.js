cc17297bc3750302c9e9e790221c6720d9e3d5e6
0hundred0 <kilgamesh@antivillain.io>
2022-10-28 17:00:03 -0400

    All theme colors added to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,48 @@ module.exports = {
 		extend: {
 			colors: {
 				primary: "#5271FF",
+				"light": {
+					"white": {
+						100: "#F9F7F7",
+						200: "#DBE2EF"
+					},
+					"blue": {
+						100: "#3F72AF",
+						200: "#112D4E",
+					},
+				},
+				"dark": {
+					"blue": {
+						100: "#BBE1FA",
+						200: "#3282B8",
+						300: "#0F4C75",
+						400: "#1B262C",
+					},
+				},
+				// <--- New Themes Color Palettes --->
+				"patriot": {
+					darkblue: "#16213E",
+					blue: "#0F3460",
+					purple: "#533483",
+					red: "#E94560"
+				},
+				"aqua": {
+					"teal": {
+						100: "#E7F6F2",
+						200: "#A5C9CA",
+						300: "#395B64",
+						400: "#2C3333"
+					}
+				},
+				// Saints Row Reference
+				"saints": {
+					"purple": {
+						100: "#372948",
+						200: "#251B37",
+					},
+					pink: "#FFCACA",
+					white: "#FFECEF"
+				}
 			},
 		},
 	},
